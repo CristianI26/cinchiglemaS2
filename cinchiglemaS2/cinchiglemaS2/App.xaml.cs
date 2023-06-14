04b6977c42b77c8e659e17b1e123baa8d023c188
master <master@SMLAB108PC19_23>
2023-06-13 21:33:59 -0500

    Agregar archivos de proyecto.

---
+++ cinchiglemaS2/cinchiglemaS2/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace cinchiglemaS2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new ejercicio();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ cinchiglemaS2/cinchiglemaS2/ejercicio.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace cinchiglemaS2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ejercicio : ContentPage
+    {
+        public ejercicio()
+        {
+            InitializeComponent();
+        }
+
+        private void pciudades_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (pciudades.SelectedIndex == -1)
+
+            {
+                DisplayAlert("alerta", "valor nulo", "cancelar");
+
+            }
+            else
+            {
+                DisplayAlert("alerta", "valor nulo", pciudades.Items[pciudades.SelectedIndex], "cancelar");
+            }
+        }
+        private void btnmostrar_clicked(object sender, EventArgs e)
+        {
+            double nota1 = Convert.ToDouble(txnota1.Text);
+            double nota2 = Convert.ToDouble(txexamen1.Text );
+            
+             
+
+
+
+        }
+    }  
+          
+        }
+
